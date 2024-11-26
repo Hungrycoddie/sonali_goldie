aa9972fe1b439799649615e2c69ecd33b497642b
Arun raj <riderarunraj@live.com>
2024-11-26 19:35:35 +0530

    switch

--- Js/condition.js
+++ Js/condition.js
@@ -1,13 +1,29 @@
-let light = "greeen";
+// let light = "greeen";
 
-if (light === "greeen"){
-    console.log("GOO");
+// if (light === "greeen"){
+//     console.log("GOO");
     
-}else if (light === "yellow"){
-    console.log("slow down");
-}else if (light === "red"){
-    console.log("Stop");
-}else {
-    console.log("Invalid signal");
-}
+// }else if (light === "yellow"){
+//     console.log("slow down");
+// }else if (light === "red"){
+//     console.log("Stop");
+// }else {
+//     console.log("Invalid signal");
+// }
+
+
+
+
+
+// switch 
+// switch(expression) {
+//     case x:
+//       // code block
+//       break;
+//     case y:
+//       // code block
+//       break;
+//     default:
+//       // code block
+//   }
 
