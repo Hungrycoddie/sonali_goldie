acf9745d7f1aff59761f6fed193aacdbeb35bdcb
Arun raj <riderarunraj@live.com>
2024-12-02 20:02:40 +0530

    update

---
+++ Js/function.js
@@ -0,0 +1,99 @@
+// function name(parameter1, parameter2, parameter3) {
+//     // code to be executed
+//   }
+// let x = 10;
+
+
+// function ar (){
+    
+// }
+
+
+// const greet = function greet (name){
+//     return `Hello, ${name}!`;
+// }
+
+// console.log(greet("Alpha"));
+
+
+// const greet = (name) => `hello ${name}`;
+// console.log(greet("Alpha"));
+
+
+
+
+
+// let name = "ar";
+// let lastname = "so";
+
+// console.log(` arun is my name ${lastname}`);
+
+
+// function add (a ,b){
+//     let a = 100;
+//     return a + b;
+// }
+
+// console.log(add(12,45));
+
+// function greet (name= "guest" ){
+//     return `hello ${name}!`;
+
+
+// }
+
+// console.log(greet());
+
+// function multiupkly (a,b){
+//     return a*b;
+
+// }
+// console.log(multiupkly(4,5));
+
+
+// setTimeout(function(){
+//     console.log("hello,AR");
+// },5000)
+
+// function calculateArea (length, width=10){
+//     return length *width
+// }
+
+// console.log(calculateArea(5));
+// console.log(calculateArea(5,15));
+
+
+
+// loops in function 
+
+
+// function sumnumbers(n){
+//     let sum = 0 ;
+//     for (let i = 1; i<=n; i++){
+//     sum += i;
+//     }
+//     return sum;
+
+// }
+// sumnumbers(5);
+
+
+// function square(num){
+//     return num * num ;
+// }
+
+// const numbers = [1,2,3,4,5];
+// console.log(message);
+
+// const squares = number.map(square);
+
+
+// console.log(squares);
+
+
+// const fruits = ["apple","banana","cherry"];
+
+// const result = fruits.toString();
+// console.log(result);
+
+
