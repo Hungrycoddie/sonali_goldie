5f84940bd948f3df1afe1f9085ccdb8650902516
Arun raj <riderarunraj@live.com>
2024-12-03 20:03:28 +0530

    objects

---
+++ Js/object.js
@@ -0,0 +1,41 @@
+// const car = {type:"Fiat", model:"500", color:"white"};
+
+
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue"
+//   };
+// console.log(person);
+
+const person = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function(a, b ) {
+      return (this.firstName + " " + this.lastName).toUpperCase() ;
+    }
+  };
+
+console.log(person.fullName());
+
+
+//   const ar = {
+//     arunvalue:"ar"
+
+//   }
+
+
+
+
+// const date = new Date()
+
+// console.log(date);
+
+
+
+
+
+
+
