c58d0d096056a12faed08308964c7d707ff990c3
Arun raj <riderarunraj@live.com>
2024-12-05 20:06:15 +0530

    lopps and objects

---
+++ Js/loops.js
@@ -0,0 +1,52 @@
+// `for( inizilization : condiition ; incre/decre){
+//     //ecicute 
+// }
+
+
+// for (let i = 0 ; i < 5 ; i++){
+//     console.log(i);
+// }
+
+
+// while (){
+//     //exicute 
+// }
+
+// let i = 0 ;
+
+// while (i<5){
+//     console.log(i);
+//     i++;
+// }
+//do while 
+
+// do {
+//     //code exicute 
+// }while (condition )
+
+
+// let i  = 10;
+
+// do {
+//     console.log(i);
+//     i++;
+// }while (i < 100)
+
+
+
+// for (let key in Object){
+    
+// }
+
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue"
+//   };
+
+
+// const colors = ["red", "green", "blue"];
+
+
+//   console.log(colors); // Output: red, green, blue
--- Js/object.js
+++ Js/object.js
@@ -9,16 +9,16 @@
 //   };
 // console.log(person);
 
-const person = {
-    firstName: "John",
-    lastName : "Doe",
-    id       : 5566,
-    fullName : function(a, b ) {
-      return (this.firstName + " " + this.lastName).toUpperCase() ;
-    }
-  };
+// const person = {
+//     firstName: "John",
+//     lastName : "Doe",
+//     id       : 5566,
+//     fullName : function(a, b ) {
+//       return (this.firstName + " " + this.lastName).toUpperCase() ;
+//     }
+//   };
 
-console.log(person.fullName());
+// console.log(person.fullName());
 
 
 //   const ar = {
@@ -35,6 +35,31 @@ console.log(person.fullName());
 
 
 
+
+
+
+
+
+
+// const person = {
+
+//   name : "arun",
+//   age : 28,
+//   city:"New york",
+// }
+
+
+
+// for (let key in person ){
+//   console.log(`${key}: ${person[key]}`);
+// }
+
+
+
+// const fruits = "Banana";
+// Object.keys(fruits).forEach(key => {
+//   console.log(`${key}: ${fruits[key]}`);
+// });
 
 
 
