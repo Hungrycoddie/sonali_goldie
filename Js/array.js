8f2ed6e0a8f6c15f0ce0a278ebd92656e2f0f5bb
Arun raj <riderarunraj@live.com>
2024-12-09 19:09:41 +0530

    uppdate

---
+++ Js/array.js
@@ -0,0 +1,46 @@
+const cars = ["Saab", "Volvo", "BMW"];
+
+
+// let car1 = "Saab";
+// let car2 = "Volvo";
+// let car3 = "BMW";
+
+// const array_name = [item1, item2, ...]; 
+
+
+// const cars = [];
+// cars[0]= "Saab";
+// cars[1]= "Volvo";
+// cars[2]= "BMW";
+// console.log(cars.sort());
+// console.log(cars.length);
+
+// let car = cars[1];
+// console.log(car);
+
+
+
+
+
+
+
+
+// array.forEach(function(currentValue, index, arr), thisValue)
+
+
+// let sum = 0;
+// const numbers = [65, 44, 12, 4];
+// numbers.forEach((item )=> {
+//     sum -= item
+// })
+
+
+// console.log(sum);
+
+// const numbers = [175, 50, 25];
+// const result = numbers.reduce((total,num)=> total - num, 200)
+
+
+// console.log(result);
+
+// // 100 (200 - 175 - 50 - 25)
