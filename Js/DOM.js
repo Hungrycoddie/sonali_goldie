9ceebfe075f284e079c0362e8aa6bfa9f746073c
Arun raj <riderarunraj@live.com>
2024-12-11 20:01:13 +0530

    dom form , event listners

--- Js/DOM.js
+++ Js/DOM.js
@@ -1,10 +1,13 @@
-const a = document.getElementById("so")
+// const a = document.getElementById("so")
 
-const firstBox = document.getElementById('example')
-console.log(example.innerText );
+// const firstBox = document.getElementById('example')
+// console.log(example.innerText );
 
 
-document.get
+// document.get
+
+
+
 
 
 
---
+++ Js/addeventlistners.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('alertButton')
+
+
+button.addEventListener('click', ()=> {
+    alert('helo ! yo have licked the button.')
+});
+
+
+function showMessage(){
+    alert('Hello this is a btton u clickked on inlline event ')
+}
