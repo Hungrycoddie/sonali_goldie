8bce85df1fc229218b01abd7e33c13ea710a27d2
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-01-24 19:09:54 +0530

    SAD

--- Angular/portfolio/src/app/app.component.ts
+++ Angular/portfolio/src/app/app.component.ts
@@ -1,28 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from "./header/header.component";
+import { TasksComponent } from "./tasks/tasks.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FormsModule, ],
+  imports: [FormsModule, HeaderComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
 
-  firstName: string = '';
-  lastName: string = '';
 
-
-
-
-  onSubmit() {
-    console.log( 'first Name:', this.firstName);
-    console.log('last Name:',this.lastName)
-
-
-    // console.log('First Name: ' + this.firstName);
-    // console.log('Last Name: ' + thi  s.lastName);
-  }
 }
